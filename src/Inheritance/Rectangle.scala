52ee277787ce5a11827da7c8bffb9f59511609aa
Shariq Khan <33172597+irfa89@users.noreply.github.com>
2020-05-13 15:29:42 -0400

    Adding Worksheets.

--- src/Inheritance/Rectangle.scala
+++ src/Inheritance/Rectangle.scala
@@ -1,11 +1,11 @@
+
 package Inheritance
-/*
+
 class Rectangle (var width:Double,var height:Double) extends Polygon 
 with Shape{
   
-  //override def area: Double = width * height // are method must be implemented
+  override def area: Double = width * height // are method must be implemented
   def color:String = "Green"
 
 }
 
-*/
